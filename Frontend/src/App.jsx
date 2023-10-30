1b85dadad6959bc4430874f47a60c01688846e1e
sumonDev13 <sumonmondal1530765@gmail.com>
2023-10-30 21:48:10 +0600

    wip frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,19 @@
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
 
-import './App.css'
-
-function App() {
-
-
+const App = () => {
   return (
     <>
-     <h1>MERN_Auth</h1>
+      <Header />
+      <ToastContainer />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
