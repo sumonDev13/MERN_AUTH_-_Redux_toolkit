f6ed2c342c3a0346bc186482f6c888dba051e0fa
sumonDev13 <sumonmondal1530765@gmail.com>
2023-10-30 19:39:34 +0600

    database connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 const app = express();
 app.use(express.json());
 
 dotenv.config();
+connectDB();
 
 const port = process.env.PORT;
 
