ced6163172bf8783ae3a72832fca5444e96a4d6f
sumonDev13 <sumonmondal1530765@gmail.com>
2023-11-04 10:56:38 +0600

    database connectiomn

---
+++ Backend-typescript/src/config/dbConnect.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import {config} from  'dotenv';
+
+config();
+
+const MONGO_URL = process?.env?.MONGO_URI;
+
+if (!MONGO_URL) {
+  throw new Error("MongoDB URI is missing in the environment variables.");
+}
+
+export const dbConnect = async () => {
+    try {
+      await mongoose.connect(MONGO_URL);
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+      process.exit(1);
+    }
+  };
--- Backend-typescript/src/index.ts
+++ Backend-typescript/src/index.ts
@@ -1,11 +1,16 @@
-import express, { Express, Request, Response , Application } from 'express';
-import dotenv from 'dotenv';
+import { config } from 'dotenv';
+import express, {  Request, Response , Application } from 'express';
+import { dbConnect } from './config/dbConnect';
 
-//For env File 
-dotenv.config();
+config();
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const port = process.env.PORT || 5000;
+
+dbConnect();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
@@ -14,3 +19,4 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
 });
+
